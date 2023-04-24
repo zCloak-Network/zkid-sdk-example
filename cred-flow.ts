f00c39a99cb611d2adb63f1ddadd9429abe5eac5
vsszhang <vsszhang@gmail.com>
2023-04-24 20:56:09 +0800

    feat: update prod env

--- cred-flow.ts
+++ cred-flow.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import * as qs from "qs";
 import * as dotenv from "dotenv";
 import { VerifiableCredential } from "@zcloak/vc/types";
-import {VerifiableCredentialBuilder} from "@zcloak/vc";
-import {RawCredential} from "@zcloak/vc/types";
+import { VerifiableCredentialBuilder } from "@zcloak/vc";
+import { RawCredential } from "@zcloak/vc/types";
 import { keys } from "@zcloak/did";
 import { Keyring } from "@zcloak/keyring";
 import { decryptMessage, encryptMessage } from "@zcloak/message";
@@ -25,13 +25,17 @@ const params = [
     receiver: "did:zk:0x4867c2Dfa7Aa14459c843d69220623cA97B652d7#key-1",
     sender: "did:zk:0x2808e410610ae6077c6291CF3582Be5EDd2023cc#key-1",
     msgType: "Request_Attestation",
-  }
+  },
 ];
 
 const keyring = new Keyring();
-const attester = keys.fromMnemonic(keyring, mnemonic, 'ecdsa');
+const attester = keys.fromMnemonic(keyring, mnemonic, "ecdsa");
 
-const attest = async (serverMsg: Message<MessageType>, num: number, deputy_num: number) => {
+const attest = async (
+  serverMsg: Message<MessageType>,
+  num: number,
+  deputy_num: number
+) => {
   // step1: decrypt claim message
   const decrypted = await decryptMessage(serverMsg, attester);
 
@@ -40,29 +44,46 @@ const attest = async (serverMsg: Message<MessageType>, num: number, deputy_num: 
 
   // step3: issuer sign a signature to build vcBuilder
   const ctypeRes = await axios.get(`${base_url}/ctype/?id=${raw.ctype}`);
-  const vcBuilder = VerifiableCredentialBuilder.fromRawCredential(raw, ctypeRes.data.data[0].rawData)
-      .setExpirationDate(null)
-      .setIssuanceDate(Date.now());
+  const vcBuilder = VerifiableCredentialBuilder.fromRawCredential(
+    raw,
+    ctypeRes.data.data[0].rawData
+  )
+    .setExpirationDate(null)
+    .setIssuanceDate(Date.now());
 
   // step4: build vc from vcBuilder
-  const vc: VerifiableCredential<false> = await vcBuilder.build(attester, false);
+  const vc: VerifiableCredential<false> = await vcBuilder.build(
+    attester,
+    false
+  );
 
   // step5: encrypt vc to message
-  const message = await encryptMessage("Response_Approve_Attestation", vc, attester, decrypted.sender, decrypted.id);
+  const message = await encryptMessage(
+    "Response_Approve_Attestation",
+    vc,
+    attester,
+    decrypted.sender,
+    decrypted.id
+  );
 
   // step6: send encrypted message to server
-  const sendRes = await axios.post(`${base_url}/message`, message);
+  const sendRes = await axios.post(
+    `${base_url}/wxBlockchainEvent/message`,
+    message
+  );
   if (sendRes.status === 200) {
-    console.log(`SUCCESS_${num}.${deputy_num}: send encrypted message to server, claim has already attested.`);
+    console.log(
+      `SUCCESS_${num}.${deputy_num}: send encrypted message to server, claim has been already attested.`
+    );
   } else {
     console.log(`send encrypted message response status: ${sendRes.status}`);
   }
-}
+};
 
 const findAndAttest = async (param: any, num: number) => {
   // initCrypto for wasm
   await initCrypto();
-  console.log('initCrypto for wasm...');
+  console.log("initCrypto for wasm...");
 
   const res = await axios.get(`${base_url}/message/?${qs.stringify(param)}`);
   if (res.data.code !== 200) {
@@ -81,7 +102,7 @@ const main = async (params: any) => {
   for (let i = 0; i < params.length; i++) {
     await findAndAttest(params[i], i);
   }
-}
+};
 
 main(params)
   .then()
--- issue-cred.ts
+++ issue-cred.ts
@@ -18,14 +18,15 @@ const base_url = process.env.BASE_URL as string;
 const mnemonic = process.env.ATTESTER_MNEMONIC as string;
 const ctypeHash = process.env.CTYPEHASH as string;
 
-const holderDidUrl: DidUrl = 'did:zk:0x2808e410610ae6077c6291CF3582Be5EDd2023cc';
+const holderDidUrl: DidUrl =
+  "did:zk:0x2808e410610ae6077c6291CF3582Be5EDd2023cc";
 const keyring = new Keyring();
 const attester = keys.fromMnemonic(keyring, mnemonic, "ecdsa");
 
 const issue = async () => {
   // initCrypto for wasm
   await initCrypto();
-  console.log('initCrypto for wasm...');
+  console.log("initCrypto for wasm...");
 
   // step0: get holder Did obj
   const didRes = await axios.get(`${base_url}/did/${holderDidUrl}`);
@@ -47,6 +48,7 @@ const issue = async () => {
   // step2: build raw
   const raw = new Raw({
     contents: {
+      id: 9870456,
       name: "vss-claimer",
     },
     owner: holderDidUrl,
@@ -55,24 +57,40 @@ const issue = async () => {
   });
 
   // step3: build rawCredential from raw
-  const rawCredential: RawCredential = raw.toRawCredential('Keccak256');
+  const rawCredential: RawCredential = raw.toRawCredential("Keccak256");
 
   // step4: build a vcBuilder by using rawCredential and ctype
-  const vcBuilder = VerifiableCredentialBuilder.fromRawCredential(rawCredential, ctype)
-      .setExpirationDate(null)
-      .setIssuanceDate(Date.now());
+  const vcBuilder = VerifiableCredentialBuilder.fromRawCredential(
+    rawCredential,
+    ctype
+  )
+    .setExpirationDate(null)
+    .setIssuanceDate(Date.now());
 
   // step5: build a vc
-  const vc: VerifiableCredential<false> = await vcBuilder.build(attester, false);
+  const vc: VerifiableCredential<false> = await vcBuilder.build(
+    attester,
+    false
+  );
 
   // step6: encrypt message
   // notice: receiverUrl parameter is holder's keyAgreement key
-  const message = await encryptMessage("Send_issuedVC", vc, attester, holder.getKeyUrl('keyAgreement'));
+  const message = await encryptMessage(
+    "Send_issuedVC",
+    vc,
+    attester,
+    holder.getKeyUrl("keyAgreement")
+  );
 
   // step7: send encrypted message to server
-  const sendRes = await axios.post(`${base_url}/message`, message);
+  const sendRes = await axios.post(
+    `${base_url}/wxBlockchainEvent/message`,
+    message
+  );
   if (sendRes.status === 200) {
-    console.log(`SUCCESS: send encrypted message to server, issue a credential to holder directly.`);
+    console.log(
+      `SUCCESS: send encrypted message to server, issue a credential to holder directly.`
+    );
   } else {
     console.log(`send encrypted message response status: ${sendRes.status}`);
   }
