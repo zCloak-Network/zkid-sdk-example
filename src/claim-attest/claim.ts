ca6d02f1bf823b5c017baccbdf30bd0f190f734f
vsszhang <vsszhang@gmail.com>
2023-06-01 16:58:32 +0800

    feat: add new demo v1.5

---
+++ src/claim-attest/claim.ts
@@ -0,0 +1,69 @@
+import path from "path";
+import dotenv from "dotenv";
+import { Raw } from "@zcloak/vc";
+import { keys } from "@zcloak/did";
+import { Keyring } from "@zcloak/keyring";
+import { initCrypto } from "@zcloak/crypto";
+import { encryptMessage } from "@zcloak/message";
+
+import type { CType } from "@zcloak/ctype/types";
+import type { DidUrl } from "@zcloak/did-resolver/types";
+
+import { getCtypeFromHash } from "../utils/ctypeHelper";
+import { sendMessage2Server } from "../utils/messageHelper";
+import { resolver } from "../utils/resolverHelper";
+import { fromDidDocument } from "@zcloak/did/did/helpers";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const holderDidUrl =
+  "did:zk:0xE5b8641d32a434BF3B5E6Ea6AFfdA1B56c558eea" as DidUrl;
+const attesterDidUrl =
+  "did:zk:0xB16FEfFaED0630F4C580a58ae0349C68609A6fDc" as DidUrl;
+
+// =====> Simulate CLAIM full process <=====
+(async () => {
+  // init crypto for wasm
+  await initCrypto();
+
+  // get holder DID from mnemonic (we need claimer's private key)
+  const keyring = new Keyring();
+  const holder = keys.fromMnemonic(
+    keyring,
+    process.env.CLAIMER_MNEMONIC as string,
+    "ecdsa"
+  );
+
+  // get attester url from DidDocument (We only need attester's public key)
+  const attesterDidDoc = await resolver.resolve(attesterDidUrl);
+  const attester = fromDidDocument(attesterDidDoc);
+
+  // Step 0: import ctype hash to construct ctype object
+  const ctype: CType = await getCtypeFromHash(process.env.CTYPEHASH);
+
+  // Step 1: claimer build raw and raw credential
+  const raw = new Raw({
+    contents: {
+      id: 9870456,
+      name: "vss-claimer",
+    },
+    owner: holderDidUrl,
+    ctype: ctype,
+    hashType: "RescuePrimeOptimized",
+  });
+  const rawCredential = raw.toRawCredential("Keccak256");
+
+  // Step 2: encrypt Request_Attestation message
+  const message = await encryptMessage(
+    "Request_Attestation",
+    rawCredential,
+    holder,
+    attester.getKeyUrl("keyAgreement"),
+    undefined,
+    resolver
+  );
+
+  // Step 3: send encrypted message to server
+  // 'message' => server => attester
+  await sendMessage2Server(message);
+})();
---
+++ src/ctype/createCtype.ts
@@ -0,0 +1,43 @@
+import { initCrypto } from "@zcloak/crypto";
+import { getPublish, getCTypeHash } from "@zcloak/ctype";
+import { Keyring } from "@zcloak/keyring";
+import { restore } from "@zcloak/did/keys";
+
+import type { BaseCType, CType } from "@zcloak/ctype/types";
+
+import { readDidKeysFile } from "../utils/didHelper";
+
+(async () => {
+  // attester
+  const keyring = new Keyring();
+  const json = readDidKeysFile();
+  const password = "12345678"; // password to decrypt your DID-keys-file
+  const attester = restore(keyring, json, password);
+
+  // initCrypto for wasm
+  await initCrypto();
+
+  // construct and publish a ctype
+  const base: BaseCType = {
+    title: "CType Create Demo",
+    description: "Demo for creating ctype",
+    type: "object",
+    properties: {
+      Name: {
+        type: "string",
+      },
+      Data_Of_Birth: {
+        type: "integer",
+        format: "timestamp",
+      },
+      Country: {
+        type: "integer",
+        format: "national-code",
+      },
+    },
+    required: [],
+  };
+  const ctype: CType = await getPublish(base, attester);
+  console.log(`ctype hash: ${getCTypeHash(base, attester.id)}`);
+  console.dir(ctype);
+})();
