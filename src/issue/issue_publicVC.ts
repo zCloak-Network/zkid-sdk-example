ba3215dc16862767996a8b4d23db137b771124fc
vsszhang <vsszhang@gmail.com>
2023-10-27 11:20:02 +0800

    refactor: optimise logic

--- src/issue/issue_publicVC.ts
+++ src/issue/issue_publicVC.ts
@@ -4,7 +4,6 @@ import { keys } from "@zcloak/did";
 import { Keyring } from "@zcloak/keyring";
 import { restore } from "@zcloak/did/keys";
 import { initCrypto } from "@zcloak/crypto";
-import { encryptMessage } from "@zcloak/message";
 import { Raw, VerifiableCredentialBuilder } from "@zcloak/vc";
 
 import type { CType } from "@zcloak/ctype/types";
@@ -12,40 +11,39 @@ import type { RawCredential } from "@zcloak/vc/types";
 import type { DidUrl } from "@zcloak/did-resolver/types";
 import type { VerifiableCredential } from "@zcloak/vc/types";
 
-import { resolver } from "../utils/resolverHelper";
 import { readDidKeysFile } from "../utils/didHelper";
-import { fromDidDocument } from "@zcloak/did/did/helpers";
 import { getCtypeFromHash, sendKycRecord } from "./util";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-// const ctypeHash =
-//   "0xeb798a4e225ca0c841d07b86453af634a4ace4fb1446b2283da4b4896846daa2";
+// TODO: only dev enviroment can run now
+// Dev .xyz
 const ctypeHash =
-  "0xbc2995791cb5b02f6a35eda411648d71bd0c1c03f9489c05d1bebbd37e2d7664";
+  "0x2c4d3f094cc8d89a6ef86a42b69741fbcf913fb4bdffa2f0c4b0d69ce293a411";
+
+// Prod .app
+// ATTENTION: using this ctypehash in prod when issuing kyc public vc
+// const ctypeHash =
+//   "0xbc2995791cb5b02f6a35eda411648d71bd0c1c03f9489c05d1bebbd37e2d7664";
+
+// ATTENTION: replace it with real KYC user's did
 const holderDidUrl: DidUrl =
-  "did:zk:0x89590750403758a34A83248A3EAA27112aee96DF";
+  "did:zk:0xE5b8641d32a434BF3B5E6Ea6AFfdA1B56c558eea";
 
 (async () => {
   // initCrypto for wasm
   await initCrypto();
   console.log("initCrypto for wasm...");
 
-  // step0: get holder and attester Did obj
-  // get holder DID from DidDocument
-  // we only need holders's public key to encrypt message
-  const holderDidDoc = await resolver.resolve(holderDidUrl);
-  const holder = fromDidDocument(holderDidDoc);
-
-  // get attester DID from DID-Keys-file
-  // we need attester's private key to sign signature when deciding to issue credential
-  //   const keyring = new Keyring();
-  //   const json = readDidKeysFile();
-  //   const password = "12345678"; // password to decrypt your DID-keys-file
-  //   const attester = restore(keyring, json, password);
-  const mnemonic =
-    "napkin patient sister puppy lake case super limb artefact alone famous edit";
+  // step0: get attester DID
+  // from DID-Keys-file
   const keyring = new Keyring();
+  // const json = readDidKeysFile();
+  // const password = "12345678"; // password to decrypt your DID-keys-file
+  // const attester = restore(keyring, json, password);
+
+  // from mnemonic
+  const mnemonic = "xxx";
   const attester = keys.fromMnemonic(keyring, mnemonic, "ecdsa");
 
   // step1: get ctype
@@ -55,11 +53,12 @@ const holderDidUrl: DidUrl =
   // Note: If the use case for VC does not involve zk computation,
   //       it is recommended to use the Keccak256 hashing method for Raw hashType.
   //       Otherwise, it is advisable to use the RescuePrimeOptimized method.
+  // ATTENTION: using this raw contents (kyc public vc ctype's struct)
   const raw = new Raw({
     contents: {
       kyc_status: 1,
       chain_code: "eth",
-      on_chain_address: "0x89590750403758a34A83248A3EAA27112aee96DF",
+      on_chain_address: "0xE5b8641d32a434BF3B5E6Ea6AFfdA1B56c558eea",
     },
     owner: holderDidUrl,
     ctype: ctype,
@@ -70,16 +69,17 @@ const holderDidUrl: DidUrl =
   const rawCredential: RawCredential = raw.toRawCredential("Keccak256");
 
   // step4: build a vcBuilder by using rawCredential and ctype
+  // ATTENTION: set expirarion date with OCR result (example is one year later in milliseconds)
   const vcBuilder = VerifiableCredentialBuilder.fromRawCredential(
     rawCredential,
     ctype
   )
-    .setExpirationDate(null)
+    .setExpirationDate(new Date().setFullYear(new Date().getFullYear() + 1))
     .setIssuanceDate(Date.now());
 
-  // step5: build a vc
+  // step5: build a public vc
+  // ATTENTION: build public vc with `true` parameter.
   const vc: VerifiableCredential<true> = await vcBuilder.build(attester, true);
-  console.dir(vc);
 
   // step6: send public VC to server
   await sendKycRecord(vc);
